cbb1a99d2c86798be0232688efee88d411bf7528
89605502155 <47472417+89605502155@users.noreply.github.com>
2020-04-07 22:28:03 +0300

    1.05

---
+++ CollisionDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public int damage = 10;
+    public string collisionTag;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == collisionTag)
+        {
+            Debug.Log("с правильным");
+        }
+        else
+        {
+            Debug.Log("с неправильным");
+        }
+        
+    }
+}
