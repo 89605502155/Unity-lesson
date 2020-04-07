15c88a50be6b7c182c64be8b1fd7fa74ac5fb3e1
89605502155 <47472417+89605502155@users.noreply.github.com>
2020-04-07 20:54:50 +0300

    1.03

---
+++ Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    public void TakeHit(int damage)
+    {
+        health -= damage;
+        if (health<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SetHealth(int health)
+    {
+        this.health += health; //this необходим, чтоб увеличить основную переменную, а не ту, что внутри метода
+
+    }
+}
